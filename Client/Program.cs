378e0916db3f6e815a7473e85a7d4799633a60a4
lammer007007 <78507776+lammer007007@users.noreply.github.com>
2021-02-09 19:40:11 +0300

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static Socket s;
+        static void Main(string[] args)
+        {
+            string welcome = "Введите имя пользователя ";
+            do
+            {
+                Console.Write(welcome);
+                string u = Console.ReadLine();
+                s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Connect(IPAddress.Parse("127.0.0.1"), 9999);
+                s.Send(Encoding.UTF8.GetBytes(u));
+                welcome = "Данное имя занято. Введите другое ";
+                Thread.Sleep(1);
+            } while(s.Available == 0);
+            s.Receive(new byte[1]);
+            string mes;
+            Task.Run(() => ReceiveMessages());
+            while(true)
+            {
+                mes = Console.ReadLine();
+                if(mes.Replace(" ", "") != "")
+                    s.Send(Encoding.UTF8.GetBytes(mes));
+            }
+        }
+
+        static void ReceiveMessages()
+        {
+            byte[] buffer = new byte[4096];
+            int count;
+            while(true)
+            {
+                count = s.Receive(buffer);
+                Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, count));
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static Dictionary<Socket, string> users = new Dictionary<Socket, string>();
+
+        static TcpListener server;
+
+        static void Main(string[] args)
+        {
+            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9999);
+            server.Start();
+            Socket temp;
+            string u;
+            byte[] buffer = new byte[4096];
+            int count;
+            Console.WriteLine("Сервер запущен");
+            while(true)
+            {
+                temp = server.AcceptSocket();
+                count = temp.Receive(buffer);
+                u = Encoding.UTF8.GetString(buffer, 0, count);
+                if(users.Values.Any(s => s == u))
+                {
+                    temp.Close();
+                    continue;
+                }
+                temp.Send(new byte[]{ 0 });
+                users.Add(temp, u);
+                Task.Run(() => ReceiveMessages(temp));
+                Console.WriteLine(u + " вошёл в чат");
+            }
+        }
+
+        static void ReceiveMessages(Socket s)
+        {
+            byte[] buffer = new byte[4096];
+            int count;
+            string mes;
+            try
+            {
+                while(true)
+                {
+                    count = s.Receive(buffer);
+                    mes = Encoding.UTF8.GetString(buffer, 0, count);
+                    foreach(var u in users.Keys)
+                        u.Send(Encoding.UTF8.GetBytes(users[s] + ": " + mes));
+                    Console.WriteLine(users[s] + ": " + mes);
+                }
+            }
+            catch(SocketException)
+            {
+                Console.WriteLine(users[s] + " покинул чат");
+                users.Remove(s);
+            }
+        }
+    }
+}
